35f61ff3cfe07818011ba794b3bc2941b575ef2b
Mukul Raghav <133975206+mukulraghav2903@users.noreply.github.com>
2024-07-11 23:00:59 +0530

    Add files via upload

---
+++ maths.js
@@ -0,0 +1,174 @@
+var quiz = {
+    "JS": [
+    {
+    "id": 1,
+    "question": "What is 6% Equals to ",
+    "options": [
+    {
+    "a": "0.06",
+    "b": "0.6",
+    "c": "0.006",
+    "d": "0.0006"
+    }
+    ],
+    "answer": "0.06",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 2,
+    "question": "How many years are there in a Decade ?",
+    "options": [
+    {
+    "a": "5 years",
+    "b": "10 years",
+    "c": "15 years"
+    }
+    ],
+    "answer": "10 years",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 3,
+    "question": "How many sides are there in a hexagon?",
+    "options": [
+    {
+    "a": "5",
+    "b": "7",
+    "c": "6"
+    }
+    ],
+    "answer": "6",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 4,
+    "question": "Solve 23 + 3 / 3",
+    "options": [
+    {
+    "a": "56",
+    "b": "24",
+    "c": "13",
+    }
+    ],
+    "answer": "24",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 5,
+    "question": "What is the Next Prime Number after 7 ",
+    "options": [
+    {
+    "a": "13",
+    "b": "11",
+    "c": "9",
+    "d": "12",
+    }
+    ],
+    "answer": "11",
+    "score": 0,
+    "status": ""
+    },
+    ]
+    }
+    var quizApp = function () {
+    this.score = 0;
+    this.qno = 1;
+    this.currentque = 0;
+    var totalque = quiz.JS.length;
+    this.displayQuiz = function (cque) {
+    this.currentque = cque;
+    if (this.currentque < totalque) {
+    $("#tque").html(totalque);
+    $("#previous").attr("disabled", false);
+    $("#next").attr("disabled", false);
+    $("#qid").html(quiz.JS[this.currentque].id + '.');
+    $("#question").html(quiz.JS[this.currentque].question);
+    $("#question-options").html("");
+    for (var key in quiz.JS[this.currentque].options[0]) {
+    if (quiz.JS[this.currentque].options[0].hasOwnProperty(key)) {
+    $("#question-options").append(
+    "<div class='form-check option-block'>" +
+    "<label class='form-check-label'>" +
+    "<input type='radio' class='form-check-input' name='option' id='q" + key + "' value='" + quiz.JS[this.currentque].options[0][key] + "'><span id='optionval'>" +
+    quiz.JS[this.currentque].options[0][key] +
+    "</span></label>"
+    );
+    }
+    }
+    }
+    if (this.currentque <= 0) {
+    $("#previous").attr("disabled", true);
+    }
+    if (this.currentque >= totalque) {
+    $('#next').attr('disabled', true);
+    for (var i = 0; i < totalque; i++) {
+    this.score = this.score + quiz.JS[i].score;
+    }
+    return this.showResult(this.score);
+    }
+    }
+    this.showResult = function (scr) {
+    $("#result").addClass('result');
+    $("#result").html("<h1 class='res-header'>Total Score: &nbsp;" + scr + '/' + totalque + "</h1>");
+    for (var j = 0; j < totalque; j++) {
+    var res;
+    if (quiz.JS[j].score == 0) {
+    res = '<span class="wrong">' + quiz.JS[j].score + '</span><i class="fa fa-remove c-wrong"></i>';
+    } else {
+    res = '<span class="correct">' + quiz.JS[j].score + '</span><i class="fa fa-check c-correct"></i>';
+    }
+    $("#result").append(
+    '<div class="result-question"><span>Q ' + quiz.JS[j].id + '</span> &nbsp;' + quiz.JS[j].question + '</div>' +
+    '<div><b>Correct answer:</b> &nbsp;' + quiz.JS[j].answer + '</div>' +
+    '<div class="last-row"><b>Score:</b> &nbsp;' + res +
+    '</div>'
+    );
+    }
+    }
+    this.checkAnswer = function (option) {
+    var answer = quiz.JS[this.currentque].answer;
+    option = option.replace(/</g, "&lt;") //for <
+    option = option.replace(/>/g, "&gt;") //for >
+    option = option.replace(/"/g, "&quot;")
+    if (option == quiz.JS[this.currentque].answer) {
+    if (quiz.JS[this.currentque].score == "") {
+    quiz.JS[this.currentque].score = 1;
+    quiz.JS[this.currentque].status = "correct";
+    }
+    } else {
+    quiz.JS[this.currentque].status = "wrong";
+    }
+    }
+    this.changeQuestion = function (cque) {
+    this.currentque = this.currentque + cque;
+    this.displayQuiz(this.currentque);
+    }
+    }
+    var jsq = new quizApp();
+    var selectedopt;
+    $(document).ready(function () {
+    jsq.displayQuiz(0);
+    $('#question-options').on('change', 'input[type=radio][name=option]', function (e) {
+    //var radio = $(this).find('input:radio');
+    $(this).prop("checked", true);
+    selectedopt = $(this).val();
+    });
+    });
+    $('#next').click(function (e) {
+    e.preventDefault();
+    if (selectedopt) {
+    jsq.checkAnswer(selectedopt);
+    }
+    jsq.changeQuestion(1);
+    });
+    $('#previous').click(function (e) {
+    e.preventDefault();
+    if (selectedopt) {
+    jsq.checkAnswer(selectedopt);
+    }
+    jsq.changeQuestion(-1);
+    });
